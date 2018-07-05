591581549fb544e1876aec9bdcd0d102a0270c57
aqqje <1042136232@qq.com>
2018-07-05 23:47:22 +0800

    add store(Actions, Mutations-Type, Mutation, State)

--- src/store/Mutations.js
+++ src/store/Mutations.js
@@ -1,6 +1,19 @@
 /*
 * 基于 State 的数据操作
 * */
+import {
+  RECEIVE_ADDRESS,
+  RECEIVE_SHOPS,
+  RECEIVE_TYPES
+} from "./Mutations-Types"
 export default {
-
+  [RECEIVE_ADDRESS](state,{address}){
+    state.address = address
+  },
+  [RECEIVE_SHOPS](state,{shops}){
+    state.shops = shops
+  },
+  [RECEIVE_TYPES](state,{types}){
+    state.types = types
+  }
 }
