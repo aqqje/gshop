56047738e262281bbe9d8018c122b3187741d588
aqqje <1042136232@qq.com>
2018-07-06 00:20:40 +0800

    updata HeaderTop title

--- src/store/Mutations-Types.js
+++ src/store/Mutations-Types.js
@@ -1,6 +1,7 @@
 /*
 * 基于 n 个 Mutation 的type 名称
 * */
-export default {
+export const RECEIVE_ADDRESS = "receive_address" // 接收地址
+export const RECEIVE_TYPES = "receive_types" // 接收地址
+export const RECEIVE_SHOPS = "receive_shops" // 接收地址
 
-}
