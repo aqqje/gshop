5e9fccca971b2c9fd24c92ed5f44a8d1edd3a12d
aqqje <1042136232@qq.com>
2018-07-11 12:11:59 +0800

    Add ShopHeader(hmtl, css)

--- src/pages/Shop/ShopInfo/ShopInfo.vue
+++ src/pages/Shop/ShopInfo/ShopInfo.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
-      ShopInfo
-    </div>
+  <div>
+    shopinfo
+  </div>
 </template>
 
 <script>
-    export default {}
+  export default {}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
